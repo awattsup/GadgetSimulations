e031c951d459fb3e328181406157c6edb8bb1439
awattsup <adamwatts1994@gmail.com>
2019-08-29 11:33:21 +0800

    fourier_decomp.py

---
+++ analysis/fourier_decomp.py
@@ -0,0 +1,36 @@
+import numpy as np 
+import pafit as pf 
+import matplotlib.pyplot as plt
+
+
+
+def main():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def create_mock_mom0():
+	dim  = 1000
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+	main()
+
+
